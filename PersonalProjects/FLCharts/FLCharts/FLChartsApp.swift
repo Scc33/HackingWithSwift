7564b1336b56823991b321af627c51ef7019cb1e
Sean Coughlin <coughlinscc33@gmail.com>
2022-01-26 15:14:09 -0600

    Experiment with other charts

---
+++ PersonalProjects/FLCharts/FLCharts/FLChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLChartsApp.swift
+//  FLCharts
+//
+//  Created by Sean Coughlin on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FLChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
